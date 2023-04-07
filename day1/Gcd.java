600fcdf1f844335400fa306f013f19ae3793239f
Rohit8008 <rohit119.be21@chitkara.edu.in>
2023-04-07 22:35:53 +0530

    First Day Learning Java

---
+++ day1/Gcd.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Gcd {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        int i=1;
+        int gcd=1;
+        while(i<=a || i<=b)
+        {
+            if(a%i==0 &&  b%i==0)
+            {
+                gcd=i;
+            }
+            i++;
+        }
+        System.out.println(gcd);
+
+    }
+}
